dcb2fcfbd5ac542eee16c5d8450dbbbc2317da8d
Mona Nashaat <nashaata@ualberta.ca>
2026-08-30 14:53:16 +0300

    update

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,7 +4,7 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <meta name="csrf-token" content="{{ csrf_token() }}">
-        <link rel="icon" type="image/x-icon" href="images/system-logo.png">
+        <link rel="icon" type="image/x-icon" href="{{ asset('images/university-logo.png') }}">
 
         <title>{{ config('app.name', 'منصة الاستبيانات') }}</title>
 
