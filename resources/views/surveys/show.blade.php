919cff750cd37d45263c4f6d0b637f4d6ccad9bf
Mona Nashaat <nashaata@ualberta.ca>
2026-08-30 14:33:56 +0300

    update

--- resources/views/surveys/show.blade.php
+++ resources/views/surveys/show.blade.php
@@ -222,7 +222,8 @@
     {{-- <div class="survey-badge">
         {{ $isCourseSurvey ? 'استبيان مرتبط بمادة' : 'استبيان عام' }}
     </div> --}}
-    <p class="survey-description">{{ {{ config('app.name', 'منصة الاستبيانات') }} }}</p>
+    <p class="survey-description">{{ config('app.name', 'منصة الاستبيانات') }}</p>
+
     {{-- <h1 class="survey-title">{{ $survey->title }}</h1> --}}
 
     {{-- @if($survey->description)
